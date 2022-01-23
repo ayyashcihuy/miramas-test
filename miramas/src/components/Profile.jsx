3f294a61928e7722c2113bd2415b8047a660a383
unknown <ayyashcihuy@gmail.com>
2022-01-23 16:18:24 +0700

    updating profile cont

--- miramas/src/components/Profile.jsx
+++ miramas/src/components/Profile.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiFillEdit } from "react-icons/ai";
+import {Switch} from "@headlessui/react"
 
 function Profile() {
+  const [enabled, setEnabled] = useState(false)
+
   return (
     <div className="profile-section grid container-sm bg-white drop-shadow-xl mx-auto my-6 rounded-lg max-w-sm">
       <div className="relative inline-block">
@@ -24,12 +27,35 @@ function Profile() {
         <div className="text-center text-gray-400 mt-2">
           <p>Layanan IT</p>
         </div>
+        <div>
         <a
           href=""
-          className="flex items-center justify-center mt-6 mr-6 text-sm text-green-800"
-        >
+          className="flex items-center justify-center mt-6 mr-6 mb-6 text-sm text-green-800"
+          >
           <AiFillEdit size={15} className="mx-2" /> Sunting Profile
         </a>
+          </div>
+          <div className="mx-12 my-6 text-gray-400 text-sm">
+            <h1>Status Perusahaan</h1>
+            <div className="flex items-center place-content-between">
+            <h1 className="text-green-800 font-bold">Aktif</h1>
+            <div className="">
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
+          relative inline-flex flex-shrink-0 h-[20px] w-[38px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          className={`${enabled ? 'translate-x-5' : 'translate-x-0'}
+            pointer-events-none inline-block h-[16px] w-[16px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+        />
+      </Switch>
+      </div>
+            </div>
+          </div>
       </div>
     </div>
   );
--- miramas/tailwind.config.js
+++ miramas/tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      borderColor: theme => ({
+        default: theme('colors.gray.300', 'currentColor'),
+        'neon': '#ccff00',
+      }),
+      backgroundColor: theme => ({
+        'neon' : '#ccff00'
+      })
+    },
+  },
+  variants: {
+    extends: {
+      backgroundColor: ['checked'],
+    }
   },
   plugins: [],
 }
